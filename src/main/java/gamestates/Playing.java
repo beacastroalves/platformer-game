2bda8736437a22c0d9d64188391181ef39290196
beatriz alves <beacastro1008@gmail.com>
2024-07-12 15:33:55 +0100

    Background That Moves ? - Episode #15

--- src/main/java/gamestates/Playing.java
+++ src/main/java/gamestates/Playing.java
@@ -5,11 +5,14 @@ import levels.LevelManager;
 import main.Game;
 import ui.PauseOverlay;
 import utilz.LoadSave;
+import static utilz.Constants.Environment.*;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class Playing extends State implements StateMethods {
   private Player player;
@@ -24,9 +27,21 @@ public class Playing extends State implements StateMethods {
   private int maxTilesOffset = lvlTilesWide - Game.TILES_IN_WIDTH;
   private int maxLvlOffsetX = maxTilesOffset * Game.TILES_SIZE;
 
+  private BufferedImage backgroundImg, bigCloud, smallClouds;
+  private int[] smallCloudsPos;
+  private Random rnd = new Random();
+
   public Playing(Game game) {
     super(game);
     initClasses();
+
+    backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.PLAYING_BG_IMG);
+    bigCloud = LoadSave.GetSpriteAtlas(LoadSave.BIG_CLOUDS);
+    smallClouds = LoadSave.GetSpriteAtlas(LoadSave.SMALL_CLOUDS);
+    smallCloudsPos = new int[8];
+    for (int i = 0; i < smallCloudsPos.length; i++) {
+      smallCloudsPos[i] = (int) (90 * Game.SCALE) + rnd.nextInt((int) (100 * Game.SCALE));
+    }
   }
 
   private void initClasses() {
@@ -67,6 +82,10 @@ public class Playing extends State implements StateMethods {
 
   @Override
   public void draw(Graphics g) {
+    g.drawImage(backgroundImg, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
+
+    drawClouds(g);
+
     levelManager.draw(g, xLvlOffset);
     player.render(g, xLvlOffset);
 
@@ -74,6 +93,16 @@ public class Playing extends State implements StateMethods {
       g.setColor(new Color(0, 0, 0, 150));
       g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
       pauseOverlay.draw(g);
+    }
+  }
+
+  private void drawClouds(Graphics g) {
+    for (int i = 0; i < 3; i++) {
+    g.drawImage(bigCloud, i * BIG_CLOUD_WIDTH - (int) (xLvlOffset * 0.3), (int) (204 * Game.SCALE), BIG_CLOUD_WIDTH, BIG_CLOUD_HEIGHT, null);
+    }
+
+    for (int i = 0; i < smallCloudsPos.length; i++) {
+    g.drawImage(smallClouds, SMALL_CLOUD_WIDTH * 4 * i- (int) (xLvlOffset * 0.7), smallCloudsPos[i], SMALL_CLOUD_WIDTH, SMALL_CLOUD_HEIGHT, null);
     }
   }
 
